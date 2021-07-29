08eec6c76c53b9f346e9808890a8073377150774
Emilio Rojas <emilio93@gmail.com>
2021-07-29 15:22:01 -0600

    Use jquery script from index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import _ from 'lodash';
+import "./script.js";
+
 import './style.css';
 import Icon from './icon.svg';
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
+  // const element = document.createElement('div');
 
-    element.appendChild(myIcon);
+  // Lodash, currently included via a script, is required for this line to work
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.classList.add('hello');
 
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  // Add the image to our existing div.
+  // const myIcon = new Image();
+  // myIcon.src = Icon;
+
+  console.log('hello');
+
+  // element.appendChild(myIcon);
+
+  // return element;
+}
+component();
+// document.body.appendChild(component());
