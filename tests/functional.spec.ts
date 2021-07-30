e1ef5319014e5e698a6cb50efc5f36fb704ed52b
Emilio Rojas <emilio.rojas@ucr.ac.cr>
2021-07-29 19:35:48 -0600

    Add functional test

---
+++ tests/functional.spec.ts
@@ -0,0 +1,48 @@
+import { test } from '@playwright/test';
+import * as path from "path";
+
+const sleep : Promise = async (ms) => {
+  return new Promise((resolve) => setTimeout(() => resolve(), ms));
+};
+
+const URL = `file:///${path.dirname(__dirname)}/docs/index.html`;
+
+test('basic test', async ({ page }) => {
+  await page.goto(URL);
+  const email = await page.$('#email');
+  const password = await page.$('#password');
+
+  await page.screenshot({ path: `screenshot-start-page.png`, fullPage: true });
+
+
+  await page.click('#login-button');
+  await sleep(2000);
+  await page.screenshot({ path: `screenshot-invalid-input.png`, fullPage: true });
+
+
+  await email.type("")
+  await password.type('miPassword');
+  await page.click('#login-button');
+  await sleep(2000);
+  await page.screenshot({ path: `screenshot-invalid-input-email.png`, fullPage: true });
+
+
+  await email.type("correo@gmail.com")
+  await password.fill('')
+  await page.click('#login-button');
+  await sleep(2000);
+  await page.screenshot({ path: `screenshot-invalid-input-password.png`, fullPage: true });
+
+  await email.type("correo@gmail.com")
+  await password.type('miPassword');
+  await sleep(2000);
+  await page.screenshot({ path: `screenshot-before-login.png`, fullPage: true });
+
+  await page.click('#login-button');
+  await sleep(2000);
+  await page.screenshot({ path: `screenshot-after-login.png`, fullPage: true });
+
+  await page.click('#logout-button');
+  await sleep(2000);
+  await page.screenshot({ path: `screenshot-after-logout.png`, fullPage: true });
+});
