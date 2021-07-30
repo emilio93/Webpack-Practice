39b534864e41f1b05f7c8bd9c33b764cddc5e4c1
Emilio Rojas <emilio.rojas@ucr.ac.cr>
2021-07-29 19:47:59 -0600

    Use development mode, source maps, and output clean

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     contentBase: './dist',
     hot: true,
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
      title: 'Development',
@@ -15,6 +17,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   module: {
     rules: [
